c7afbea65f70d2416b0c3d4e240be262e3e056e3
WhiteLion97 <mojo.slovynskyvgmail.com>
2023-12-18 08:35:06 +0100

    upload website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://xbegan00.github.io',
+});
